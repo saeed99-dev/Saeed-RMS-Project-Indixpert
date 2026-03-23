bae57e6657eb48900c0a3c5b34bd513bdad6717e
saeed99-dev <saeedhabib1999@gmail.com>
2026-03-23 12:23:50 +0530

    modified accroding to changes in Login and Signup pages

--- app/src/dashboard/user_dashboard_manager.py
+++ app/src/dashboard/user_dashboard_manager.py
@@ -3,26 +3,14 @@ from src.auth.manage_user import user_menu
 from src.auth.signup_page import Signup
 from src.auth.login_page import Login
 from src.security.validation import Validator
-from src.filehandling.filemode import Filemode
-
-file = Filemode()
-all_user = file.load_data()
-valid = Validator()
 
 def user_menu_dashobard():
     user_menu()
     while True:
+        valid = Validator()
         option = valid.validoption()
         if option == 1:
-            admin_found = False
-            for user in all_user:
-                if user.get("role") == "Admin":
-                    admin_found = True
-                    break
-            if admin_found == False:
-                Signup().admin()
-            else:
-                Signup().staff()
+            Signup().staff()
         elif option == 2:
             Login().login()
         elif option == 3:
@@ -33,4 +21,4 @@ def user_menu_dashobard():
             print("\nGoodbye!")
             break
         else:
-            print("PLease select option carefully!")
+            print("PLease select option carefully(option > 0)!")
