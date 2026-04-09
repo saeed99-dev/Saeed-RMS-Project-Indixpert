a97a5a809ed45aa99d77c1db192a0925aeeee191
saeed99-dev <saeedhabib1999@gmail.com>
2026-04-09 20:20:21 +0530

    decorate with color and font style

--- app/src/utils/tools.py
+++ app/src/utils/tools.py
@@ -1,38 +1,71 @@
-import time
+from datetime import datetime
+from src.filehandling.filemode import Filemode
+from src.model.models import PathModel
 import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
 
 def exit_program(operation,massage):
-    print(operation, end="")
-    for _ in range(5):
-        time.sleep(0.25)
-        print(".", end="")
-    print(f"\n{massage}")
+    try:
+        print(f"{Fore.GREEN}{operation}", end="", flush=True)
+        for _ in range(5):
+            time.sleep(0.25)
+            print(f"{Fore.GREEN}.", end="", flush=True)
+
+        print(f"\n{Fore.YELLOW}{massage}{Style.RESET_ALL}")
+    except Exception as e:
+        exit_data={
+            "error": str(e),
+            "time": str(datetime.now()),
+            "function name": "exit_program",
+        }
+        Filemode().append_data(exit_data,PathModel().security_log)
+        print(f"\n{operation}...\n{massage}")
     
 
-def display_canvas_logo(title,subtitle,date):
-    width = 80
-    print("╔" + "═" * (width - 2) + "╗")
-    print("║" + " " * (width - 2) + "║")
-    print("║" + title.center(width - 2) + "║")
-    print("║" + subtitle.center(width - 2) + "║")
-    print("║" + date.center(width - 2) + "║")
-    print("║" + " " * (width - 2) + "║")
-    print("╚" + "═" * (width - 2) + "╝")
+def display_canvas_logo(title, subtitle, date):
+    print("\n" + Fore.CYAN + "╔" + "═" * 78 + "╗")
+    print(f"{Fore.CYAN}{'║':<78} {'║'}")
+    print(f"{Fore.CYAN}{'║':<31}{Fore.WHITE}{title}{Fore.CYAN}{'║':>31}")
+    print(f"{Fore.CYAN}{'║':<31}{Fore.WHITE}{subtitle}{Fore.CYAN}{'║':>31}")
+    print(f"{Fore.CYAN}║{Fore.WHITE}{date:^78}{Fore.CYAN}║")
+    print(f"{Fore.CYAN}{'║':<78} {'║'}")
+    print(Fore.CYAN + "╚" + "═" * 78 + "╝")
 
-def sub_heading(text):
-    width=80
-    print("╔" + "═" * (width - 2) + "╗")
-    print("║" + text.center(width - 2) + "║")
-    print("╚" + "═" * (width - 2) + "╝")
 
-def canvas():
-    art=f"{Fore.RED}C{Fore.GREEN} A{Fore.YELLOW} N{Fore.BLUE} V{Fore.MAGENTA} A{Fore.CYAN} S{Fore.WHITE}"
-    # print(art)
-    #back=f"{Back.RED} C {Back.GREEN} A {Back.YELLOW} N {Back.BLUE} V {Back.MAGENTA} A {Back.CYAN} S {Back.WHITE}"
-    # print(back)
+
+def main_subheading(text):
+    canvas_fore = f"{Fore.WHITE}[ {Fore.RED}C{Fore.GREEN} A{Fore.YELLOW} N{Fore.BLUE} V{Fore.MAGENTA} A{Fore.CYAN} S {Style.RESET_ALL}]"
+    full_text = f"{text} :  {canvas_fore}"
+
+    print("\n" + Fore.CYAN + "╔" + "═" * 78 + "╗")
+    print(f"{Fore.CYAN}║{Fore.YELLOW}{full_text:>94}{Fore.CYAN}{'║':>24}")
+    print(Fore.CYAN +"╚" + "═" * 78 + "╝")
+
+def staff_subheading(text):
+    canvas_fore = f"{Fore.WHITE}[ {Fore.RED}C{Fore.GREEN} A{Fore.YELLOW} N{Fore.BLUE} V{Fore.MAGENTA} A{Fore.CYAN} S {Style.RESET_ALL}]"
+    full_text = f"{text} :  {canvas_fore}"
+
+    print("\n" + Fore.BLUE + "╔" + "═" * 78 + "╗")
+    print(f"{Fore.BLUE}║{Fore.YELLOW}{full_text:>94}{Fore.BLUE}{'║':>24}")
+    print(Fore.BLUE +"╚" + "═" * 78 + "╝")
+
+def admin_subheading(text):
+    canvas_fore = f"{Fore.WHITE}[ {Fore.RED}C{Fore.GREEN} A{Fore.YELLOW} N{Fore.BLUE} V{Fore.MAGENTA} A{Fore.CYAN} S {Style.RESET_ALL}]"
+    full_text = f"{text} :  {canvas_fore}"
+
+    print("\n" + Fore.GREEN + "╔" + "═" * 78 + "╗")
+    print(f"{Fore.GREEN}║{Fore.YELLOW}{full_text:>94}{Fore.GREEN}{'║':>24}")
+    print(Fore.GREEN +"╚" + "═" * 78 + "╝")
+
+def connect_subheading(text):
+    canvas_fore = f"{Fore.WHITE}[ {Fore.RED}C{Fore.GREEN} A{Fore.YELLOW} N{Fore.BLUE} V{Fore.MAGENTA} A{Fore.CYAN} S {Style.RESET_ALL}]"
+    full_text = f"{text} :  {canvas_fore}"
+
+    print("\n" + Fore.BLUE + "╔" + "═" * 78 + "╗")
+    print(f"{Fore.BLUE}║{Fore.YELLOW}{full_text:>94}{Fore.BLUE}{'║':>24}")
+    print(Fore.BLUE +"╠" + "═" * 78 + "╣")
 
 def press_to_continue(press_button,return_page):
     input(f"\n Press [{press_button}] to return to the {return_page} ")
@@ -40,4 +73,4 @@ def press_to_continue(press_button,return_page):
 # display_canvas_logo("C A N V A S","THE ART OF DINING", "Est. 2026")
 # exit_program("Exiting","GoodBye!")
 # press_to_continue("Enter","Dashboard")
-canvas()
+# canvas()
